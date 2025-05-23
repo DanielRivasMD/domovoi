6f0f6648437bf499a9b12bcb4c5386708249f26d
Daniel Rivas <danielrivasmd@gmail.com>
2025-05-23 06:52:44 +0200

    add file exist

---
+++ fileExist.go
@@ -0,0 +1,44 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+package domovoi
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/DanielRivasMD/horus"
+)
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// FileExist checks if a file exists. If it doesn't, it executes the provided
+// notFoundAction. It returns a tuple: a boolean indicating whether the check is successful
+// (either the file exists or the custom action adequately handled the missing file)
+// and an error carrying detailed diagnostic context (if any). The verbose flag enables optional logging.
+func FileExist(filePath string, notFoundAction horus.NotFoundAction, verbose bool) (bool, error) {
+	_, err := os.Stat(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			if verbose {
+				fmt.Printf("File '%s' does not exist. Executing custom action.\n", filePath)
+			}
+			if notFoundAction != nil {
+				ok, actionErr := notFoundAction(filePath)
+				if actionErr != nil {
+					return false, horus.NewHerror("check file", fmt.Sprintf("'%s' not found action failed", filePath), actionErr, map[string]any{"path": filePath})
+				}
+				return ok, nil
+			}
+			return false, horus.NewHerror("check file", fmt.Sprintf("file '%s' not found", filePath), nil, map[string]any{"path": filePath, "action": "none"})
+		}
+		return false, horus.NewHerror("check file", fmt.Sprintf("error checking file '%s'", filePath), err, map[string]any{"path": filePath})
+	}
+	if verbose {
+		fmt.Printf("File '%s' exists.\n", filePath)
+	}
+	return true, nil
+}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
