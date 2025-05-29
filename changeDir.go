9792f32eaaf4b518645c26e9e016dd5f95459f86
Daniel Rivas <danielrivasmd@gmail.com>
2025-05-29 12:32:08 +0200

    update change dir

--- changeDir.go
+++ changeDir.go
@@ -9,6 +9,7 @@ import (
 	"os"
 
 	"github.com/DanielRivasMD/horus"
+	"github.com/ttacon/chalk"
 )
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -17,20 +18,20 @@ import (
 func ChangeDir(newDir string) error {
 	err := os.Chdir(newDir)
 	if err != nil {
-		// Create a categorized error using horus
+		// Create a categorized error using horus with a colored message.
 		herr := horus.NewCategorizedHerror(
 			"change directory",
 			"directory_error",
-			fmt.Sprintf("failed to change directory to '%s'", newDir),
+			chalk.Red.Color(fmt.Sprintf("failed to change directory to '%s'", newDir)),
 			err,
 			map[string]any{"target_directory": newDir},
 		)
 
-		// Optionally, log the error for debugging
+		// Optionally, log the error for debugging.
 		fmt.Println(horus.FormatError(herr, horus.JSONFormatter))
 		return herr
 	}
 	return nil
 }
 
-///////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////
